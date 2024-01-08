5194fcf87f8c69807b7c9b24fd8dd581a6a16342
RenkasVajra <twinka4ik174@mail.ru>
2024-01-08 20:37:03 +0500

    add convertergit add .!

--- my_test/staticfiles/scripts/demand_script.py
+++ my_test/staticfiles/scripts/demand_script.py
@@ -3,23 +3,51 @@ import pandas as pd
 import math
 
 
+
+def convert_salary(row):
+    if row['salary_currency'] != 'RUB':
+        currency = row['salary_currency']
+        salary_from = row['salary_from']
+        salary_to = row['salary_to']
+        exchange_rate = row[currency]
+
+        if pd.notnull(salary_from):
+            salary_from_rub = salary_from * exchange_rate
+            row['salary_from'] = salary_from_rub
+
+        if pd.notnull(salary_to):
+            salary_to_rub = salary_to * exchange_rate
+            row['salary_to'] = salary_to_rub
+
+    return row
+
+
 def demand():
     selected_profession = 'Специалист по информационной безопасности'
     csv_file = "C:/Users/Dich/Desktop/VYZ/django-study/vacancies.csv"
     data = pd.read_csv(csv_file, usecols=['name', 'salary_from', 'salary_to', 'salary_currency', 'published_at'])
-    data = data.fillna(0)
-    data['Год'] = pd.to_datetime(data['published_at'], utc=True).dt.year
-    data['average_sal'] = data[['salary_from', 'salary_to']].mean(axis=1, skipna=True).apply(math.floor).astype(
+    data['published_at'] = pd.to_datetime(data['published_at'], utc=True).dt.strftime("%Y-%m")
+    data = data.replace('RUR', 'RUB')
+    conv = pd.read_csv('C:/Users/Dich/Downloads/currency.csv')
+    data = data.dropna(subset=['salary_currency'])
+    data[['salary_from', 'salary_to']] = data[['salary_from', 'salary_to']].fillna(value=0)
+    merged_df = pd.merge(data, conv, left_on='published_at', right_on='date', how='left')
+
+    converted_df = merged_df.apply(convert_salary, axis=1)
+    converted_df['salary_currency'] = converted_df['salary_currency'].fillna('RUB')
+    converted_df = converted_df.fillna(0)
+    converted_df['Год'] = pd.to_datetime(converted_df['published_at'], utc=True).dt.year
+    converted_df['average_sal'] = converted_df[['salary_from', 'salary_to']].mean(axis=1, skipna=True).apply(math.floor).astype(
         int)
-    years = sorted(data['Год'].unique())
-    average_salary_all = data.groupby('Год')['average_sal'].mean().apply(math.floor).astype(
+    years = sorted(converted_df['Год'].unique())
+    average_salary_all = converted_df.groupby('Год')['average_sal'].mean().apply(math.floor).astype(
         int).to_frame()  # зарплата по годам
-    selected_profession_data = data[data.name.str.contains(selected_profession, na=False, case=False)]
+    selected_profession_data = converted_df[converted_df.name.str.contains(selected_profession, na=False, case=False)]
     selected_salary_all = selected_profession_data.groupby('Год')[
         'average_sal'].mean().astype(
         int).to_frame()  # зарплата по годам выбранная
 
-    years_df = data.groupby('Год')['name'].count().reset_index()  # кол-во вакансий по годам
+    years_df = converted_df.groupby('Год')['name'].count().reset_index()  # кол-во вакансий по годам
     vacancies_all_count = years_df['name'].tolist()
     vacancies_count_profession_by_year = selected_profession_data.groupby('Год')['name'].count().reset_index()
     vacancies_count_profession_by_year = vacancies_count_profession_by_year.rename(
