4aefd8a8b978899b8b3342a9da198ec3eb2d2635
RenkasVajra <twinka4ik174@mail.ru>
2024-01-17 18:59:18 +0500

    edit to regru

--- my_test/my_test/wsgi.py
+++ my_test/my_test/wsgi.py
@@ -13,8 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 
 # -*- coding: utf-8 -*-
-sys.path.insert(0, '/var/www/u2439865/data/www/issecurityaoo.ru/django-study')
-sys.path.insert(1, '/var/www/u2439865/data/djangoenv/lib/python3.9/site-packages')
+sys.path.insert(0, '/var/www/u2439865/data/www/issecurityaoo.ru/django-study/my_test')
+sys.path.insert(1, '/var/www/u2439865/data/www/issecurityaoo.ru/django-study/my_test/djangoenv/lib/python3.9/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'my_test.settings'
 application = get_wsgi_application()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_test.settings')
